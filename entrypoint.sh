321e05b6e9f85e089cb2ead42628dfa57d86b1b2
Tulio Monte Azul <tuliomonteazul@gmail.com>
2017-07-15 17:52:49 -0300

    Change entry point to add MYSQL_FORCE_DUMP

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,11 @@ shopt -s nullglob
 OUTPUT=/docker-entrypoint-initdb.d/mysql-dump.sql
 DATADIR=/var/lib/mysql
 
-# if DATADIR is empty
-if ! test "$(ls -A "$DATADIR")"; then
+IS_DATADIR_EMPTY=$(ls -A "$DATADIR")
+echo -e "IS_DATADIR_EMPTY: ${IS_DATADIR_EMPTY}"
+echo -e "MYSQL_FORCE_DUMP:  ${MYSQL_FORCE_DUMP}"
+# if DATADIR is empty or dump load is being forced
+if [[ ! -z "${IS_DATADIR_EMPTY}" ]] || [[ "${MYSQL_FORCE_DUMP}" == "true" ]]; then
 	if [ ! -z "${MYSQL_DUMP_URL}" ]; then
 		echo -e "\nDownloading from ${MYSQL_DUMP_URL} to ${OUTPUT}...\n"
 		curl -pSL -o ${OUTPUT} "${MYSQL_DUMP_URL}"
